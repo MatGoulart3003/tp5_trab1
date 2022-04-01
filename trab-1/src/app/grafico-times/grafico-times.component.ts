58d17b1cd79137340be44d77ecd71a4e7d90c5fe
Mateus Goulart <mat_goulart3003@yahoo.com.br>
2022-04-01 20:06:28 -0300

    grafico funcionando

--- trab-1/src/app/grafico-times/grafico-times.component.ts
+++ trab-1/src/app/grafico-times/grafico-times.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ColunaModel } from "src/models/colunaModel"
+import { Team } from '../CadastroTimes/team';
+import { ListaTimesComponent } from '../lista-times/lista-times.component';
 
 @Component({
   selector: 'app-grafico-times',
@@ -10,21 +12,61 @@ export class GraficoTimesComponent implements OnInit {
 
   public colunas : Array<ColunaModel> = [];
 
+  teamList: Team []=[] 
+  valores: any = []
+  total = 0 
+  porcentagem: any = []
+  
+
   constructor() { }
 
   ngOnInit(): void {
+    this.catchData();
+    this.populaValores();
+
+
+
+    
     this.carregaColunas();
+  }
+
+  populaValores (){
+
+    for(let i = 0; i < this.teamList.length; i++){
+
+      this.valores[i] = this.teamList[i].votes
+      this.total = this.total + this.valores[i]
+      
+    }
+    console.log(this.total)
+
+    for(let i = 0; i < this.valores.length; i++){
+
+      var xuxa = (this.valores[i] / this.total) * 100
+      this.porcentagem[i] = xuxa.toFixed(2)
+    }
+
+  }
+
+  catchData (){    
+
+    if(localStorage.getItem("BD-Times")){          
+      this.teamList = JSON.parse(localStorage.getItem("BD-Times")!)
+
+    }else{
+      this.teamList = []
+    }
+
   }
 
   carregaColunas() {
     var lista = [];
-    var valores = [60, 9, 100, 20, 75, 33, 39, 50, 18, 59, 33, 44, 12, 10]
-
-    for (let index = 0; index < valores.length ; index++) {
+    
+    for (let index = 0; index < this.porcentagem.length ; index++) {
       var coluna = new ColunaModel();
-      coluna.porcentagem = valores[index];
-      coluna.descricao = " Coluna - bar - " + index;
-
+      coluna.porcentagem = this.porcentagem[index];
+      coluna.descricao = this.teamList[index].name;
+      
       lista.push(coluna);
     }
 
@@ -36,5 +78,8 @@ export class GraficoTimesComponent implements OnInit {
   {
     return valor + "%";
   }
+
+
+
 
 }
